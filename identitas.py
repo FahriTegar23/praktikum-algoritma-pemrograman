bbb5532033d5f9945c557fc21e0931325e796f8d
FahriTegar23 <fahritegar23@gmail.com>
2026-08-21 17:10:16 +0700

    Add files via upload

---
+++ identitas.py
@@ -0,0 +1,10 @@
+# Meanmpilkan identitas mahasiswa
+print("\t\t=== Identitas Mahasiswa ===")
+# Menampilkan Nama Mahasiswa
+print("Nama\t: Fahri Tegar Maulana")
+# Menampilkan NIM Mahasiswa
+print("NIM\t\t: 2605060076")
+# Menampilkan Tempat dan Tanggal Lahir, Jenis Kelamin, dan Hobi Mahasiswa
+print("Tempat, Tanggal Lahir\t: Temanggung, 10 Desember 2007")
+print("Jenis Kelamin\t: Laki-laki")
+print("Hobi\t: Bermain Game")
