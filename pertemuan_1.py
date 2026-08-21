b3193b6409aeeb9fbe7c8609065f0f7c26bbb7fa
FahriTegar23 <fahritegar23@gmail.com>
2026-08-21 18:59:51 +0700

    Add files via upload

---
+++ pertemuan_1.py
@@ -0,0 +1,7 @@
+#Contoh 1
+print("Hello, World!");
+print("Ini program pertama saya.");
+#Contoh 2
+print("\t\tHalo Apa Kabar?");
+print("\n Selamat Datang di Lab. Teknologi Informasi");
+print("\n Universitas Tidar");
