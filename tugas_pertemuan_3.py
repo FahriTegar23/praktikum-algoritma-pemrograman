973ea58b1305e2b48c3b0fecb781d70564fa94a3
FahriTegar23 <fahritegar23@gmail.com>
2026-09-04 16:56:56 +0700

    Add files via upload

---
+++ tugas_pertemuan_3.py
@@ -0,0 +1,19 @@
+# Data bangunan 
+print("Data Bangunan")
+panjang = 12
+lebar = 5
+tinggi = 8
+
+# Menghitung luas, volume, dan keliling bangunan
+luas = 2 * ((panjang * lebar) + (panjang * tinggi) + (lebar * tinggi))
+volume = panjang * lebar * tinggi
+keliling = 4 * (panjang + lebar + tinggi)
+
+# Menampilkan hasil perhitungan
+print("Luas =", luas)
+print("Volume =", volume)
+print("Keliling =", keliling)
+
+# Operasi Komperasi
+print("Apakah luas lebih besar dari 50?", luas > 50)
+print("Apakah volume  tersebut bernilai 480?", volume == 480)
